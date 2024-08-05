95f58562c175e1f2cd8d61d895d92f35ffeecea5
yash badgujar <yashbadgujar2023@gmail.com>
2024-08-05 23:35:37 +0530

    Add the wallpaper grid

--- lib/Screen/HomePage.dart
+++ lib/Screen/HomePage.dart
@@ -28,42 +28,80 @@ class _HomepageState extends State<Homepage> {
                       fontSize:
                           shortside * 0.04 < 25 ? 30 : shortside * 0.04))),
         ),
-        body: Column(
-          // mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Center(
-              child: Container(
-                width: width > 700 ? width * 0.5 : width,
-                height: height * 0.05,
-                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
-                clipBehavior: Clip.antiAlias,
-                decoration: BoxDecoration(),
-                child: TextField(
-                  readOnly: true,
-                  decoration: InputDecoration(
-                    hintText: "Search...",
-                    hintStyle: TextStyle(
-                        color: Theme.of(context).brightness == Brightness.dark
-                            ? Colors.white
-                            : Colors.black),
-                    filled: true,
-                    fillColor: Theme.of(context).brightness == Brightness.dark
-                        ? Colors.white38
-                        : Colors.black38,
-                    suffixIcon: Icon(
-                      Icons.search,
-                      color: Theme.of(context).brightness == Brightness.dark
-                          ? Colors.white
-                          : Colors.black,
+        body: Container(
+          width: width,
+          child: SingleChildScrollView(
+            child: Column(
+              // mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Center(
+                  child: Container(
+                    width: width > 700 ? width * 0.5 : width,
+                    height: height * 0.05,
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                    clipBehavior: Clip.antiAlias,
+                    decoration: BoxDecoration(),
+                    child: TextField(
+                      readOnly: true,
+                      decoration: InputDecoration(
+                        hintText: "Search...",
+                        hintStyle: TextStyle(
+                            color:
+                                Theme.of(context).brightness == Brightness.dark
+                                    ? Colors.white
+                                    : Colors.black),
+                        filled: true,
+                        fillColor:
+                            Theme.of(context).brightness == Brightness.dark
+                                ? Colors.white38
+                                : Colors.black38,
+                        suffixIcon: Icon(
+                          Icons.search,
+                          color: Theme.of(context).brightness == Brightness.dark
+                              ? Colors.white
+                              : Colors.black,
+                        ),
+                        border: OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.all(Radius.circular(8))),
+                      ),
                     ),
-                    border: OutlineInputBorder(
-                        borderSide: BorderSide.none,
-                        borderRadius: BorderRadius.all(Radius.circular(8))),
                   ),
                 ),
-              ),
+                SizedBox(
+                  height: shortside * 0.05,
+                ),
+                Card(
+                  child: Container(
+                    margin: EdgeInsets.symmetric(horizontal: width * 0.02),
+                    child: GridView.builder(
+                        shrinkWrap: true,
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: (width / 200).toInt() - 1,
+                            childAspectRatio: 9 / 16,
+                            mainAxisSpacing: shortside * 0.015,
+                            crossAxisSpacing: shortside * 0.015),
+                        itemCount: 300,
+                        itemBuilder: (context, index) {
+                          return Container(
+                            decoration: BoxDecoration(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(10)),
+                              color: Colors.white,
+                            ),
+                            clipBehavior: Clip.antiAlias,
+                            child: Image.network(
+                              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6oXkR5VjWbZ3cnP0bxzzpzVQkpHdOOzG5cw&s",
+                              fit: BoxFit.cover,
+                            ),
+                          );
+                        }),
+                  ),
+                )
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
